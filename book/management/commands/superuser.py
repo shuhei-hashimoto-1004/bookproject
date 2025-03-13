7fefb56c471716a42bf177ac5ba5e93732bc43c0
shuhei-hashimoto-1004 <shuhashimoto3@gmail.com>
2025-03-13 18:36:15 +0900

    修正

--- book/management/commands/superuser.py
+++ book/management/commands/superuser.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import get_user_model
+from django.contrib.auth import get_user_model
 from django.conf import settings
 
 User = get_user_model()
