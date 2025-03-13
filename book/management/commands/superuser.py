1d049ee4d03812ff289ba74f8ca89e7b43386535
shuhei-hashimoto-1004 <shuhashimoto3@gmail.com>
2025-03-13 18:43:07 +0900

    キー隠す

--- book/management/commands/superuser.py
+++ book/management/commands/superuser.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from django.conf import settings
+import os
 
 User = get_user_model()
 
@@ -8,9 +9,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username='test taro').exists():
             User.objects.create_superuser(
-                username='test taro',
+                username=os.environ.get('SUPERUSER_USERNAME'),
                 email='',
-                password='P@ssword0123'
+                password=os.environ.get('SUPERUSER_PASS')
             )
             self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
         else:
--- bookproject/settings.py
+++ bookproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2+(uo^++o8oatl%1!v7uucm+4z8@^9dn^$-0-_9mipezwvah)j'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
